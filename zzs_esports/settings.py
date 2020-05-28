77194d4068ac1315e3624e68274aa354156f49fa
T0mpak <musaacuminatagenep@yandex.com>
2020-05-29 01:35:13 +0600

    My Django Girls app, third commit

--- zzs_esports/settings.py
+++ zzs_esports/settings.py
@@ -121,11 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = (
-    #os.path.join(BASE_DIR, 'static'),
-#)
+STATIC_ROOT = '/home/t0mpak/projects/site/assets/'
+#STATIC_DIR = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
